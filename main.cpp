347bc38f5c56550e4aa42d4bf311f067189c428d
DAChenScratch <73948853+DAChenScratch@users.noreply.github.com>
2021-01-30 18:49:46 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,39 +1,39 @@
-
-//Check the http_stuff for credits
-
+//HTTP credits: http://lcs.ios.ac.cn/~maxt/SPelton/reports/report-9aa0d3.html
+//JSON credits: https://github.com/nlohmann/json
 #include <iostream>
+#include "./json.hpp"
 #include "./http_stuff.h"
 using namespace std;
+using namespace nlohmann;
 int main(void) {
-  cout << "Server waking up...";
   httplib::Server svr;
-  //Get /, that is how the battlesnake engine will customize your snake.
   svr.Get("/", [](const auto &, auto &res) {
-    string head = "default"; //TODO: Customize
-    string tail = "default"; //TODO: Customize
-    string author = ""; //TODO: Customize
-    string color = "#888888"; //TODO: Customize (any hex color)
+    string head = "default"; //TODO: Change head
+    string tail = "default"; //TODO: Change tail
+    string author = ""; //TODO: Change your battlesnake username
+    string color = "#888888";  //TODO: Change a hex color
     res.set_content("{\"apiversion\":\"1\", \"head\":\"" + head + "\", \"tail\":\"" + tail + "\", \"color\":\"" + color + "\", " + "\"author\":\"" + author + "\"}", "text/json");
   });
-  //Sent by the battlesnake engine when it needs you to move
-  svr.Post("/move", [](auto &req, auto &res){
-    //get json data
-    string data = req.body;
-    cout << "\n\n" + data;
-    string moves[4] = {"up", "down", "left", "right"};
-    //TODO: Make some snake logic
-    string move = "right";
-    res.set_content("{\"move\": \"" + move + "\"}", "text/plain");
-  });
-  //Purely functional, useful if you have a stateful snake
   svr.Post("/end", [](const auto &, auto &res){
     res.set_content("ok", "text/plain");
   });
-  //Also purely functional unless you have a stateless snake.
   svr.Post("/start", [](const auto &, auto &res){
     res.set_content("ok", "text/plain");
   });
-  //Listen on host 0.0.0.0 and port 8080
-  //Repl.it http servers will only run if the host is 0.0.0.0
+  svr.Post("/move", [](auto &req, auto &res){
+    const json data = json::parse(req.body);
+    cout << data;
+    cout << "\n\n";
+    //You can get the "you" property like this:
+    //data["you"];
+    //Almost alike python dictionary parsing, but with a semicolon at the end of each line.
+    //You might need to make some structs to store some data in a variable
+    //Example:
+    //you_struct you = data["you"];
+    string moves[4] = {"up", "down", "left", "right"};
+    int index = rand() % 4;
+    res.set_content("{\"move\": \"" + moves[index] + "\"}", "text/plain");
+  });
   svr.listen("0.0.0.0", 8080);
+  cout << "Server started";
 }
